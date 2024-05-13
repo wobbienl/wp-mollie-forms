4d764a8faba1076cc7f13c453cfb2bdc4057f9b5
Nick Dijkstra <nick@goedgepickt.nl>
2024-05-13 10:26:08 +0200

    Version 2.6.13

--- classes/MollieForms.php
+++ classes/MollieForms.php
@@ -9,7 +9,7 @@ class MollieForms
     /**
      * Plugin version number
      */
-    const PLUGIN_VERSION = '2.6.12';
+    const PLUGIN_VERSION = '2.6.13';
 
     /**
      * Webhook URL for Mollie
